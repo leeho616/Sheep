e72e296af03f30ca1433ce5295f0c4bf496b58c8
leeho616 <1420003847@qq.com>
2024-08-30 10:56:12 +0800

    Add files via upload

---
+++ MyFirst.java
@@ -0,0 +1,10 @@
+public class MyFirst{
+		 public static void main(String[] args){
+			 String brand="华为荣耀30pro"；
+					double weight=12.4;
+			 			String color="土豪金电池"；
+			 					int price=3000;
+			 			System.out.print("手机品牌:"brand+","+"重量:"+weight","+"颜色；"）
+			 					
+		   }
+      }
